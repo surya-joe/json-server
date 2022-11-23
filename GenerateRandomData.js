708f2bed370a08c4f96988833661c4580a970b33
surya-joe <suryajjoe@gmail.com>
2022-11-23 20:41:28 +0530

    init

---
+++ GenerateRandomData.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+    const data = {
+        products:[],
+    }
+    for(let i=0; i<1000; i++){
+        data.products.push(
+            {
+                id: i,
+                title: `Product ${i}`
+            }
+        )
+    }
+    return data 
+} 
